0b616bf3d050de7b7d8494ee77cefafe03a2475a
Mavetoo <mavertoo@ya.ru>
2022-03-28 00:26:33 +0500

    First Cod

--- GoodProject/ViewController.swift
+++ GoodProject/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var helloWorldLabel: UILabel! // Заголовок
+    @IBOutlet var loggleButton: UIButton! // Кнопка
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loggleButton.layer.cornerRadius = 10
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func makeButtonAcrion() {
+        if helloWorldLabel.isHidden == false {
+            helloWorldLabel.isHidden = true
+            loggleButton.setTitle("Показать", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = false
+            loggleButton.setTitle("Скрыть", for: .normal)
+        }
+        
+        
+    }
+    
 }
 
